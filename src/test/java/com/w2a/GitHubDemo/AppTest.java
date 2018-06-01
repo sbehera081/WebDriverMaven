fa16f4b94c0421fbb5da706bfa777fa93d7d65cf
Sunanda <sbehera081@gmail.com>
2018-06-01 22:15:09 +0530

    initial comment

---
+++ src/test/java/com/w2a/GitHubDemo/AppTest.java
@@ -0,0 +1,23 @@
+package com.w2a.GitHubDemo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	
+	WebDriver driver;
+
+	@Test
+	public void verifySearch() {
+		String DriverPath = "F:\\Selenium\\Jar Files\\geckodriver-v0.18.0-win64\\";
+		System.setProperty("webdriver.gecko.driver", DriverPath+"geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.get("http://only-testing-blog.blogspot.in/");
+		driver.close();
+	}
+
+}
